e1a92442e23837b433dfecd843bf83f096ea486c
dumiduzee <dumidulk@outlook.com>
2025-06-20 23:05:46 +0530

    Add endpoints for creating and retrieving data from Redis

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ redis_client = Redis(host="redis",port=6379,db=0,decode_responses=True)
 @app.get("/")
 def root():
     return "Hello world"
+
+
+@app.get("/create")
+def crete():
+    redis_client.set("name","dumidu")
+
+
+
+@app.get("/love")
+def new():
+    data = redis_client.get("name")
+    return data
+
+
